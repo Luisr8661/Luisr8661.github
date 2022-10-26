4193343add921692f0c39362d856453e8d570dc0
Luisr8661 <115160496+Luisr8661@users.noreply.github.com>
2022-10-26 15:36:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Luis R"; // update the part BETWEEN the "quotes"
+let userName = "Luisr8661"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
